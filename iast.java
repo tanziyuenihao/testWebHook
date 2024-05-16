e328101daec67b4ba491aa382b1f6c429914e844
tanziyuenihao <46347791+tanziyuenihao@users.noreply.github.com>
2024-05-16 16:34:55 +0800

    Create iast.java

---
+++ iast.java
@@ -0,0 +1,20 @@
+public interface EsIndexConstant {
+
+    String VUL_COMPONENT = "vul_component";
+
+    String VUL = "vul";
+
+    String COMPONENT_LICENSE = "license";
+
+    String COMPONENT_LICENSE_NEW = "license";
+
+    String SIM_RESULT = "sim_result";
+
+    String SIM_RESULT_DETAIL = "sim_result_detail";
+
+    String COMPONENT_VERSION = "component_*";
+
+    String COMPONENT_BINARY_APP = "component_binary_app";
+
+    String iast = "test123123";
+}
